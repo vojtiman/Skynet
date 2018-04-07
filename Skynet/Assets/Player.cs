d63bb8ce38f818a53692f8fa86ce0ef012d96ad2
rluks <rluks@seznam.cz>
2018-04-07 15:34:54 +0200

    controlling the tube

--- Skynet/Assets/Player.cs
+++ Skynet/Assets/Player.cs
@@ -12,6 +12,10 @@ public class Player : MonoBehaviour {
     bool requestLeft;
     bool requestRight;
 
+    bool requestMid;
+    bool sideReached;
+    bool returning;
+
     // Use this for initialization
     void Start () {
         Tube = TubeGO.transform;
@@ -19,7 +23,11 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    if(Input.GetKeyDown(KeyCode.LeftArrow))
+
+        if (sideReached && !returning)
+            StartGoMiddle();
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             requestLeft = true;
         }
@@ -55,13 +63,40 @@ public class Player : MonoBehaviour {
         StartCoroutine(GoToTarget(target));
     }
 
+    void StartGoMiddle()
+    {
+        returning = true;
+        StopAllCoroutines();
+        Vector3 mid = new Vector3(0, Tube.position.y, Tube.position.z);
+        StartCoroutine(GoMid(mid));
+    }
+
     IEnumerator GoToTarget(Vector3 target)
     { 
         while (true)
         {
             Tube.position = Vector3.MoveTowards(Tube.position, target, Speed * Time.smoothDeltaTime);
             if (Tube.position == target)
+            {
+                sideReached = true;
                 yield return null;
+            }       
+
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    IEnumerator GoMid(Vector3 mid)
+    {  
+        while (true)
+        {
+            Tube.position = Vector3.MoveTowards(Tube.position, mid, Speed * Time.smoothDeltaTime);
+            if (Tube.position == mid)
+            {
+                sideReached = false;
+                returning = false;
+                yield return null;
+            }
 
             yield return new WaitForSeconds(0.01f);
         }
