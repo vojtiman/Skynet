513100fb1309ec184143b4059004e2c53ef8e296
rluks <rluks@seznam.cz>
2018-04-07 08:54:46 +0200

    GO hierarchy

---
+++ Skynet/Assets/Spawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject headTemplate;
+    public GameObject Heads;
+
+	// Use this for initialization
+	void Start () {
+        GameObject newHead = Instantiate(headTemplate, transform.position, Quaternion.identity) as GameObject;
+        newHead.name = "Head";
+        newHead.transform.parent = Heads.transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
