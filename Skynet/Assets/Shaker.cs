247f9d5b87ddd34080608af1af57411d27f2efa9
rluks <rluks@seznam.cz>
2018-04-07 14:24:40 +0200

    attempt to shake heads

---
+++ Skynet/Assets/Shaker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shaker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.attachedRigidbody) {
+            if(Random.Range(0, 100) > 90)
+            {
+                float x = Random.Range(-10, 10);
+                float y = Random.Range(-200, 10);
+                float z = Random.Range(-10, 10);
+                Vector3 r = new Vector3(x, y, z);
+                other.attachedRigidbody.AddForce(r * 100);
+            }         
+        }
+            
+    }
+}
