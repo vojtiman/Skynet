2864f0726ed4e100e8ce62738f7ebc84319afa17
rluks <rluks@seznam.cz>
2018-04-07 14:51:30 +0200

    wip player to control tube

---
+++ Skynet/Assets/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public GameObject Tube;
+
+    bool requestLeft;
+    bool requestRight;
+
+    public float TargetXPosition;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            requestLeft = true;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            requestRight = true;
+        }
+
+        if (requestLeft == requestRight)
+            return;
+
+        if (requestLeft)
+            StartCoroutine(GoLeft());
+
+        if (requestRight)
+            StartCoroutine(GoRight());
+    }
+
+    IEnumerator GoLeft()
+    {
+        yield return null;
+    }
+
+    IEnumerator GoRight()
+    {
+        yield return null;
+    }
+}
