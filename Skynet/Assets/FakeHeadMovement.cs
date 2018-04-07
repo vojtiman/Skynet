0965017805a25b6c4f932316663e93a543aaec96
rluks <rluks@seznam.cz>
2018-04-07 16:55:33 +0200

    scripted movement of head through the tube

--- Skynet/Assets/FakeHeadMovement.cs
+++ Skynet/Assets/FakeHeadMovement.cs
@@ -4,18 +4,56 @@ using UnityEngine;
 
 public class FakeHeadMovement : MonoBehaviour {
 
-    Valve v;
+    public Valve v;
+    public GameObject HeadsWaitingGO;
+    public float Speed;
 
-	// Use this for initialization
-	void Start () {
+    bool moving;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (v.IsOpen)
+        if (!moving && v.IsOpen && HeadsWaitingGO.transform.childCount > 0)
         {
-
+            moving = true;
+            GameObject head = HeadsWaitingGO.transform.GetChild(0).gameObject;
+            StartMoveHead(head);
         }
 	}
+
+    void StartMoveHead(GameObject head)
+    {
+        head.transform.parent = transform.parent;
+
+        head.GetComponent<Collider>().isTrigger = true;
+
+        var rb = head.GetComponent<Rigidbody>();
+        rb.useGravity = false;
+        rb.isKinematic = true;      
+        
+        StartCoroutine(MoveHead(head));
+    }
+
+    IEnumerator MoveHead(GameObject head)
+    {
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            Vector3 target = transform.GetChild(i).position;
+            while (true)
+            {
+                head.transform.position = Vector3.MoveTowards(head.transform.position, target, Speed * Time.smoothDeltaTime);
+                if (head.transform.position == target)
+                {
+                    break;
+                    //yield return null;
+                }
+
+                yield return new WaitForSeconds(0.01f);
+            }          
+        }
+    }
 }
